9769a4c8e18ee609ddd40df8f340c6e835a4828a
Sahil Mulani <40736800+SahillMulani@users.noreply.github.com>
2022-05-12 19:02:33 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,36 +1,61 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Employee {
     String name;
     int id;
     String gender;
+    String designation;
 
-    public Employee(String name, int id, String gender) {
+    public Employee(String name, int id, String gender, String designation) {
         this.name = name;
         this.id = id;
         this.gender = gender;
+        this.designation = designation;
     }
 
-    public static void filter(int n, Employee e[]) {
+    public static void filter(int n, ArrayList<Employee> e) {
+
+        ArrayList<Employee> MaleManager = new ArrayList<Employee>();
+        ArrayList<Employee> FemaleManager = new ArrayList<Employee>();
+        ArrayList<Employee> MaleExecutive = new ArrayList<Employee>();
+        ArrayList<Employee> FemaleExecutive = new ArrayList<Employee>();
+
         for (int i = 0; i < n; i++) {
-            if (e[i].gender.equals("Male")) {
-                System.out.println("y");
-                System.out.println(e[i].gender);
-            } else {
-                System.out.println("x");
-                System.out.println(e[i].gender);
+            if (e.get(i).gender.equals("Male") && e.get(i).designation.equals("Manager")) {
+                MaleManager.add(e.get(i));
+            }
+            if (e.get(i).gender.equals("Male") && e.get(i).designation.equals("Executive")) {
+                MaleExecutive.add(e.get(i));
+            }
+            if (e.get(i).gender.equals("Female") && e.get(i).designation.equals("Manager")) {
+                FemaleManager.add(e.get(i));
+            }
+            if (e.get(i).gender.equals("Female") && e.get(i).designation.equals("Executive")) {
+                FemaleExecutive.add(e.get(i));
             }
         }
-
+        for (int i = 0; i < MaleManager.size(); i++) {
+            System.out.println(MaleManager.get(i).name);
+        }
+        for (int i = 0; i < FemaleManager.size(); i++) {
+            System.out.println(FemaleManager.get(i).name);
+        }
+        for (int i = 0; i < MaleExecutive.size(); i++) {
+            System.out.println(MaleExecutive.get(i).name);
+        }
+        for (int i = 0; i < FemaleExecutive.size(); i++) {
+            System.out.println(FemaleExecutive.get(i).name);
+        }
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the number of employee");
         int n = in.nextInt();
-        Employee e[] = new Employee[n];
+        ArrayList<Employee> e = new ArrayList<Employee>();
         for (int i = 0; i < n; i++) {
-            e[i] = new Employee(in.next(), in.nextInt(), in.next());
+            e.add(new Employee(in.next(), in.nextInt(), in.next(), in.next()));
         }
 
         filter(n, e);
